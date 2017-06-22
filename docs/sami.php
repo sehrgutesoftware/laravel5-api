9f91f0cedfc89cc8ceb3b6d10c022e819ec45eb9
Joseph Paul <mail@jsph.pl>
2017-06-22 19:57:40 +0200

    Prepare docs update

--- docs/sami.php
+++ docs/sami.php
@@ -10,7 +10,7 @@ $iterator = Symfony\Component\Finder\Finder::create()
     ->in($dir);
 
 $versions = Sami\Version\GitVersionCollection::create($dir)
-    ->add('v0.4.0', 'Master');
+    ->add('v0.4.1', 'Master');
 
 $options = [
     'theme'                => 'default',
