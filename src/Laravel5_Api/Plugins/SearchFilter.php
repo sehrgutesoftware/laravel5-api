26f44327066653a08c9a26757373f1234155fd81
Joseph Paul <mail@jsph.pl>
2017-06-23 11:36:57 +0200

    SearchFilter plugin: Enable search on related fields

--- src/Laravel5_Api/Plugins/SearchFilter.php
+++ src/Laravel5_Api/Plugins/SearchFilter.php
@@ -31,6 +31,32 @@ class SearchFilter extends Plugin implements AdaptCollectionQuery
      * - searchable: An array of fieldnames on the model to compare with
      * - search_param: Name of the request parameter that holds the search query
      *
+     * Example for searching only on a model's local attributes:
+     *
+     * ```
+     * 'searchable' => [
+     *     'name',
+     *     'description',
+     *     'bio'
+     * ]
+     * ```
+     *
+     * Example for searching also on the fields of a model's relationships:
+     *
+     * ```
+     * 'searchable' => [
+     *     'name',
+     *     'description',
+     *     'posts' => [  // Search the following fields on the `post` relation
+     *         'title',
+     *         'body'
+     *         'comments' => [  // Supports recursive nesting
+     *             'message'
+     *         ]
+     *     ]
+     * ]
+     * ```
+     *
      * @var array
      */
     protected $default_config = [
@@ -63,12 +89,42 @@ class SearchFilter extends Plugin implements AdaptCollectionQuery
     protected function applyFilter(Builder $query, Array $fields, String $condition)
     {
         $query->where(function($subquery) use ($fields, $condition) {
-            foreach ($fields as $field) {
-                $subquery->orWhere($field, 'ilike', "%$condition%");
+            foreach ($fields as $relation => $field) {
+                $subquery = $this->addCondition($subquery, $relation, $field, $condition);
             }
             return $subquery;
         });
 
+        return $query;
+    }
+
+    /**
+     * Add a where filter to the `$query`. When `$field` is an array, the fields
+     * in the array will be compared against `$relation`. This works recursively.
+     *
+     * @param Builder $query            The query on which to apply the "wheres"
+     * @param String|Int  $relation     The relation on which to compare (ignored if $field is not an array)
+     * @param String|Array  $field      Name of the field (or fields)
+     * @param String  $condition        The search term
+     */
+    protected function addCondition(Builder $query, $relation, $field, String $condition)
+    {
+        if (is_array($field)) {
+            // Recursive search on relations
+            $that = $this;
+            $query->orWhereHas($relation, function($subquery) use ($that, $condition, $field) {
+                $subquery->where(function($subsubquery) use ($that, $condition, $field) {
+                    foreach ($field as $subrelation => $subfield) {
+                        $subsubquery = $that->addCondition($subsubquery, $subrelation, $subfield, $condition);
+                    }
+                    return $subsubquery;
+                });
+                return $subquery;
+            });
+        } else {
+            // Search on a model attribute
+            $query->orWhere($field, 'ilike', "%$condition%");
+        }
         return $query;
     }
 }
